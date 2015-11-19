d2426e21bb351cb16543d66d38a597461cddabd2
zzhao <zhongda.zhao@vwd.com>
2015-11-19 09:55:50 +0100

    first taste of testing with spock

---
+++ src/test/groovy/com.example.component/ObservableQueueSpec.groovy
@@ -0,0 +1,27 @@
+/*
+* vwd KL
+* Created by zzhao on 11/19/15 9:34 AM
+*/
+package com.example.component
+
+import spock.lang.Specification
+
+/**
+ * @author zzhao
+ */
+class ObservableQueueSpec extends Specification {
+
+    def "observer based observable queue should work"() {
+        given:
+        def ooq = new ObservableQueue<Integer>()
+        def result = []
+        ooq.observe().subscribe { i -> result << i }
+
+        when:
+        ooq.offer(1)
+        ooq.offer(2)
+
+        then:
+        "[1, 2]".equals(result.toString())
+    }
+}
